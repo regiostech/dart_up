8c1ca790f600340783672bcc53aabf9fc52e8cde
Tobe O <thosakwe@gmail.com>
2019-09-23 21:43:30 -0400

    /add endpoint

--- lib/src/commands/serve.dart
+++ lib/src/commands/serve.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+import 'dart:isolate';
 import 'package:angel_framework/angel_framework.dart';
 import 'package:angel_framework/http.dart';
 import 'package:args/command_runner.dart';
 import 'package:dart_up/src/models.dart';
 import 'package:logging/logging.dart';
+import 'package:path/path.dart' as p;
 import 'package:pretty_logging/pretty_logging.dart';
+import 'package:pubspec_parse/pubspec_parse.dart';
 
 class ServeCommand extends Command {
   @override
@@ -35,8 +39,37 @@ class ServeCommand extends Command {
     app.get('/list', (req, res) => apps);
 
     app.post('/add', (req, res) async {
-
+      var appDill = req.uploadedFiles.firstWhere(
+          (f) => f.contentType.mimeType == 'application/dill',
+          orElse: () => throw FormatException(
+              'Missing application/dill file in payload.'));
+      var pubspecYamlFile = req.uploadedFiles.firstWhere(
+          (f) => f.contentType.mimeType == 'text/yaml',
+          orElse: () =>
+              throw FormatException('Missing text/yaml file in payload.'));
+      var pubspecYaml = await pubspecYamlFile.readAsString();
+      var pubspec = Pubspec.parse(pubspecYaml);
+      // Kill existing application, if any.
+      await apps.remove(pubspec.name)?.kill();
+      // Download the dependencies into a temp dir.
+      var tempDir = await Directory.systemTemp.createTemp();
+      // req.shutdownHooks.add(() => tempDir.delete(recursive: true));
+      var pubspecFile = File(p.join(tempDir.path, 'pubspec.yaml'));
+      await pubspecFile.writeAsString(pubspecYaml);
+      var pub = await Process.run('pub', ['get', '--no-precompile']);
+      // TODO: Handle error
+      var packagesFile = File(p.join(tempDir.path, '.packages'));
+      // Save the dill file, and spawn an isolate.
+      var dillFile = File(p.join(tempDir.path, 'app.dill'));
+      await appDill.data.pipe(dillFile.openWrite());
+      // Use the temp dir's .packages file
+      // TODO: Handle exits
+      var isolate = await Isolate.spawnUri(dillFile.uri, [], null);
+      var appModel = Application()..isolate = isolate;
+      return appModel;
     });
+
+    app.fallback((req, res) => throw AngelHttpException.notFound());
 
     await http.startServer(
         argResults['address'], int.parse(argResults['port'] as String));
--- lib/src/models.dart
+++ lib/src/models.dart
@@ -1,12 +1,18 @@
+import 'dart:async';
 import 'dart:isolate';
 
 class Application {
   Isolate isolate;
-  bool isDead;
+  bool isDead = false;
 
   Application();
 
   Application.fromJson(Map m) : isDead = m['is_dead'] == true;
+
+  Future<void> kill() async {
+    isolate.kill();
+    isDead = true;
+  }
 
   Map<String, dynamic> toJson() {
     return {
