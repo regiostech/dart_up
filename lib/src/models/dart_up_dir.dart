790d198914f145bd08e9d0ec83b71c558abaae13
Tobe O <thosakwe@gmail.com>
2019-09-24 17:07:56 -0400

    Add password validator

--- lib/src/models/dart_up_dir.dart
+++ lib/src/models/dart_up_dir.dart
@@ -1,20 +1,69 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
+import 'package:dbcrypt/dbcrypt.dart';
 import 'package:path/path.dart' as p;
 import 'application_directory.dart';
 
 class DartUpDirectory {
   final Directory directory;
+  final dbCrypt = DBCrypt();
   DartUpAppsDirectory _appsDir;
+  DartUpPasswordFile _passwordFile;
 
   DartUpDirectory(this.directory);
+
+  DartUpAppsDirectory get appsDir => _appsDir ??=
+      DartUpAppsDirectory(Directory(p.join(directory.path, 'apps')));
+
+  DartUpPasswordFile get passwordFile => _passwordFile ??=
+      DartUpPasswordFile(dbCrypt, File(p.join(directory.path, 'passwords')));
 
   Future<void> initialize() async {
     await appsDir.directory.create(recursive: true);
   }
+}
 
-  DartUpAppsDirectory get appsDir => _appsDir ??=
-      DartUpAppsDirectory(Directory(p.join(directory.path, 'apps')));
+class DartUpPasswordFile {
+  final DBCrypt dbCrypt;
+  final File file;
+
+  DartUpPasswordFile(this.dbCrypt, this.file);
+
+  Future<bool> verify(String username, String password) async {
+    var pws = await read();
+    var pw = pws[username];
+    if (pw == null) return false;
+    return dbCrypt.hashpw(password, pw.salt) == pw.hashedPassword;
+  }
+
+  Future<Map<String, DartUpPassword>> read() async {
+    var out = <String, DartUpPassword>{};
+    if (await file.exists()) {
+      var lines =
+          file.openRead().transform(utf8.decoder).transform(LineSplitter());
+      await for (var line in lines) {
+        var parts =
+            line.split(':').map((s) => s.trim()).where((s) => s.isNotEmpty);
+        if (parts.length == 3) {
+          var pw = DartUpPassword.fromParts(parts.toList());
+          out[pw.username] = pw;
+        }
+      }
+    }
+    return out;
+  }
+}
+
+class DartUpPassword {
+  String username, salt, hashedPassword;
+
+  DartUpPassword(this.username, this.salt, this.hashedPassword);
+
+  DartUpPassword.fromParts(List<String> parts)
+      : username = parts[0],
+        salt = parts[1],
+        hashedPassword = parts[2];
 }
 
 class DartUpAppsDirectory {
