e8542f8cd91dec5fe06a964cdbc9f0fd3e5005e9
Tobe O <thosakwe@gmail.com>
2019-09-24 12:14:26 -0400

    Install command (not done/tested)

--- lib/src/commands/install.dart
+++ lib/src/commands/install.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 import 'package:args/command_runner.dart';
+import 'package:cli_util/cli_util.dart';
+import 'package:path/path.dart' as p;
 
 class InstallCommand extends Command {
   @override
@@ -26,6 +28,9 @@ class InstallCommand extends Command {
   }
 
   run() async {
+    var user = argResults['user'] as String;
+    var workingDirectory = argResults['working-directory'] as String;
+    var pubPath = p.join(getSdkPath(), 'bin', 'pub');
     var file = File(argResults['out'] as String);
     await file.writeAsString('''
 [Unit]
